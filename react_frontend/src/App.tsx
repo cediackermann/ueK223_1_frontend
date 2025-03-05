425eb79f75f7ab5b21898e8967fef68f1ce0037b
Cedric Ackermann <ackermanncedi@gmail.com>
2025-03-05 14:36:42 +0100

    Refactor App component to include BrowserRouter for routing support

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
-import { ActiveUserContextProvider } from './Contexts/ActiveUserContext';
-import Router from './Router/Router';
+import { ActiveUserContextProvider } from "./Contexts/ActiveUserContext";
+import Router from "./Router/Router";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <ActiveUserContextProvider>
-      <Router />
-    </ActiveUserContextProvider>
+    <BrowserRouter>
+      <ActiveUserContextProvider>
+        <Router />
+      </ActiveUserContextProvider>
+    </BrowserRouter>
   );
 }
 
